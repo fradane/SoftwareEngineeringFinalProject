ed4af63c77d237a4169a11c5f9c06c27b65418b4
Francesco Danese <francescodanese@icloud.com>
2025-03-04 16:48:06 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.polimi.ingsw.is25am33 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.polimi.ingsw.is25am33 to javafx.fxml;
+    exports it.polimi.ingsw.is25am33;
+}
