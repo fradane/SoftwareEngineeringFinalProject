2e9db90f80fede7ecb9e5ec74ceed5b8ef52d6f1
marcodene <mdenegri03@gmail.com>
2025-03-18 09:15:57 +0100

    Fix bugs

--- src/main/java/it/polimi/ingsw/is25am33/model/board/Level1ShipBoard.java
+++ src/main/java/it/polimi/ingsw/is25am33/model/board/Level1ShipBoard.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.is25am33.model.board;
 
 import it.polimi.ingsw.is25am33.model.Direction;
-import it.polimi.ingsw.is25am33.model.crew.CrewMember;
+import it.polimi.ingsw.is25am33.model.CrewMember;
 import it.polimi.ingsw.is25am33.model.dangerousObj.DangerousObj;
 
 import java.util.List;
