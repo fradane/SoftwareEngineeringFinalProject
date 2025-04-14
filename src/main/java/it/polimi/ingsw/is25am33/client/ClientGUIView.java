de0444cd2dc8ebe9bdf6c09a2dcab33c87ac75d6
fradane <francesco.danese@mail.polimi.it>
2025-04-14 22:42:41 +0200

    Fix post merge

--- src/main/java/it/polimi/ingsw/is25am33/client/ClientGUIView.java
+++ src/main/java/it/polimi/ingsw/is25am33/client/ClientGUIView.java
@@ -1,8 +1,5 @@
 package it.polimi.ingsw.is25am33.client;
 
-import it.polimi.ingsw.is25am33.client.ClientView;
-import it.polimi.ingsw.is25am33.model.GameState;
-import it.polimi.ingsw.is25am33.model.game.GameInfo;
 
 public abstract class ClientGUIView implements ClientView {
 
