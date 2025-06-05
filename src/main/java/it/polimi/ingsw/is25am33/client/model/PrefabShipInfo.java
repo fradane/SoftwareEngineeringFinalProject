6ff21d521c9f8d5981d7a6a48bd0bf319ebf24bf
marcodene <mdenegri03@gmail.com>
2025-06-05 16:50:39 +0200

    Fixed prefabShip for socket

--- src/main/java/it/polimi/ingsw/is25am33/client/model/PrefabShipInfo.java
+++ src/main/java/it/polimi/ingsw/is25am33/client/model/PrefabShipInfo.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.is25am33.client.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 
 public class PrefabShipInfo implements Serializable {
@@ -8,7 +10,12 @@ public class PrefabShipInfo implements Serializable {
     private final String description;
     private final boolean forTestFlight;
 
-    public PrefabShipInfo(String id, String name, String description, boolean forTestFlight) {
+    @JsonCreator
+    public PrefabShipInfo(
+            @JsonProperty("id") String id, 
+            @JsonProperty("name") String name, 
+            @JsonProperty("description") String description, 
+            @JsonProperty("forTestFlight") boolean forTestFlight) {
         this.id = id;
         this.name = name;
         this.description = description;
